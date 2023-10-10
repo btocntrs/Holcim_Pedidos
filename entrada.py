c15ca5b0d2c964b5e84b93d58951e4e49ecd9adf
Alberto Contreras <btocntrs@gmail.com>
2023-10-10 10:04:45 -0600

    Se modificó el archivo entrada para que la Clase Entrada generara una entrada personalizada dependiendo de si al descripción del producto corresponde a Hielo, Agua o pago de Carga y descarga, ya que el calculo de precio o cantidad de Hielo varían dependiendo del tipo de producto.
    También se modificó el método str para generar una salida por consola personalizada y ahorrar código.

--- entrada.py
+++ entrada.py
@@ -1,11 +1,33 @@
 class Entrada:
-    def __init__(self, pedido, fecha, descripcion, cantidad, precio, importe, planta, nota):
+    def __init__(self, pedido, fecha, descripcion, cantidad, importe, planta, nota):
         self.pedido = pedido
         self.fecha = fecha
         self.descripcion = descripcion
-        self.cantidad = cantidad
-        self.importe = importe
         self.planta = planta
-        self.nota = nota
+        self.nota = nota       
         
+        if "Servicio" in descripcion:
+            self.importe = importe * 1.16
+            self.precio = get_precio_servicio(planta)
+            self.cantidad = importe / self.precio
+        else:
+            self.cantidad = cantidad
+            self.importe = importe
+            self.precio = self.importe / self.cantidad
     
+    # Imprimimos la entrada    
+    def __str__(self) -> str:
+        return f"""{self.pedido}  {self.fecha} {self.descripcion}   {self.cantidad}      {self.precio}      {self.importe}   {self.planta}    {self.nota}
+        --------------------------------------------------------------------------------------------------------"""
+
+
+    # Devolvemos el precio del servicio de carga y descarga dependiente de la planta       
+    def get_precio_servicio(planta):
+        if 'Esc' in planta:
+            return 0.7308
+        elif 'Cande' in planta:
+            return 1.2296
+        elif 'Chetu' in planta:
+            return 2.0532
+        else:
+            return 0.0
