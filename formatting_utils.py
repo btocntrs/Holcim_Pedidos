f9f85a80f35ee942e0e23d047899904c4490cf67
Alberto Contreras <btocntrs@gmail.com>
2023-10-12 09:19:41 -0600

    Se creo el modulo formatting_utils para incluir todos los método que tengan que ver con el formateo de texto.
    Dentro del módulo hay un método llamado formato_flotantes que formatea los flotantes a un texto más fácil de leer.
    Los miles están separados por comas y los decimales por un punto.

---
+++ formatting_utils.py
@@ -0,0 +1,5 @@
+# Este método formatea las cantidades separando los miles por comas y los flotantes con un punto.
+# El métdoo devuelve texto
+@staticmethod
+def formato_flotantes(cantidad) -> str:
+    return "{:,.2f}".format(cantidad)
