aecb8aa5ef4f112730693dd7b8853c6b1bb7a5e5
Alberto Contreras <btocntrs@gmail.com>
2023-10-12 09:26:34 -0600

    Se agregaron comentarios

--- formatting_utils.py
+++ formatting_utils.py
@@ -1,5 +1,5 @@
 # Este método formatea las cantidades separando los miles por comas y los flotantes con un punto.
-# El métdoo devuelve texto
+# El métdoo devuelve texto.
 @staticmethod
 def formato_flotantes(cantidad) -> str:
     return "{:,.2f}".format(cantidad)
